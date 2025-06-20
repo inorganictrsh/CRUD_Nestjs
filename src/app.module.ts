59ea9c11fd7bed8d137c9bbe7aefbdacea70d395
Saritaidk18diehb <pipe19075@gmail.com>
2025-06-20 15:40:41 -0500

    enviroment setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TaskModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USER'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/**/entitys/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
---
+++ src/task/task.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { TaskController } from './task.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([])],
+  controllers: [TaskController],
+  providers: [TaskService],
+})
+export class TaskModule {}
